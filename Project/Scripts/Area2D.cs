eceee80df6807d80a81c92fcf116cd969c917dbc
loprocks <skiboots221@gmail.com>
2023-09-29 17:12:36 -0400

    push for nic

--- Project/Scripts/Area2D.cs
+++ Project/Scripts/Area2D.cs
@@ -10,36 +10,36 @@ public partial class Area2D : Godot.Area2D
 	public Vector2 ScreenSize; // Size of the game window.
 	
 	public override void _Ready()
-{
-	ScreenSize = GetViewportRect().Size;
-}
+	{
+		ScreenSize = GetViewportRect().Size;
+	}
 	
 	
 	
 	public override void _Process(double delta)
-{
+	{
 	
-	var velocity = Vector2.Zero; // The player's movement vector.
+		var velocity = Vector2.Zero; // The player's movement vector.
 
-	if (Input.IsActionPressed("move_right"))
-	{
-		velocity.X += 1;
-	}
+		if (Input.IsActionPressed("move_right"))
+		{
+			velocity.X += 1;
+		}
 
-	if (Input.IsActionPressed("move_left"))
-	{
-		velocity.X -= 1;
-	}
+		if (Input.IsActionPressed("move_left"))
+		{
+			velocity.X -= 1;
+		}
 
-	if (Input.IsActionPressed("move_down"))
-	{
-		velocity.Y += 1;
-	}
+		if (Input.IsActionPressed("move_down"))
+		{
+			velocity.Y += 1;
+		}
 
-	if (Input.IsActionPressed("move_up"))
-	{
-		velocity.Y -= 1;
-	}
+		if (Input.IsActionPressed("move_up"))
+		{
+			velocity.Y -= 1;
+		}
 
 	
 }
