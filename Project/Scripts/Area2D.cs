73490a8608afbd5eca0045a2c0ff27e5737ec0f2
loprocks <skiboots221@gmail.com>
2023-09-29 17:45:31 -0400

    tile map & player actually moves

--- Project/Scripts/Area2D.cs
+++ Project/Scripts/Area2D.cs
@@ -6,7 +6,7 @@ public partial class Area2D : Godot.Area2D
 {
 	[Export]
 	public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec).
-
+	
 	public Vector2 ScreenSize; // Size of the game window.
 	
 	public override void _Ready()
@@ -14,16 +14,15 @@ public partial class Area2D : Godot.Area2D
 		ScreenSize = GetViewportRect().Size;
 	}
 	
-	
-	
 	public override void _Process(double delta)
 	{
-	
+		
 		var velocity = Vector2.Zero; // The player's movement vector.
-
+		
 		if (Input.IsActionPressed("move_right"))
 		{
 			velocity.X += 1;
+			
 		}
 
 		if (Input.IsActionPressed("move_left"))
@@ -40,7 +39,15 @@ public partial class Area2D : Godot.Area2D
 		{
 			velocity.Y -= 1;
 		}
-
+		if (velocity.Length() > 0)
+   		{
+		velocity = velocity.Normalized() * Speed;
+		}	
+		Position += velocity * (float)delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+		);
 	
 }
 
