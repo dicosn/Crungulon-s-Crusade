57ca8a56d7baaca580bfea282374592dd66ea612
loprocks <skiboots221@gmail.com>
2023-10-03 17:46:17 -0400

    10_3_2023
    Script does not work but collision works and tilemap has been better understood

--- Project/Scripts/Area2D.cs
+++ Project/Scripts/Area2D.cs
@@ -4,9 +4,21 @@ using System;
 public partial class Area2D : Godot.Area2D
 
 {
+	[Signal]
+	public delegate void HitEventHandler();
+	private void _on_body_entered(Node2D body)
+{
+	Hide();
+}
+	private void _on_body_exited(Node2D body)
+{
+	Show();
+}
 	[Export]
 	public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec).
-	
+	//public int Gravity { get; set; } = 4000; // How fast the player will fall
+	// get gravity from project settings (keep everything synced)
+	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 	public Vector2 ScreenSize; // Size of the game window.
 	
 	public override void _Ready()
@@ -14,40 +26,45 @@ public partial class Area2D : Godot.Area2D
 		ScreenSize = GetViewportRect().Size;
 	}
 	
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
 		
-		var velocity = Vector2.Zero; // The player's movement vector.
+		Vector2 velocity = new Vector2(); // The player's movement vector.
 		
 		if (Input.IsActionPressed("move_right"))
 		{
-			velocity.X += 1;
-			
+			velocity.X += 40;
 		}
 
 		if (Input.IsActionPressed("move_left"))
 		{
-			velocity.X -= 1;
-		}
+			velocity.X -= 40;
+		}		
+		//velocity.Y += Gravity;
+		
+		MoveAndSlide();
+		
+		
 
-		if (Input.IsActionPressed("move_down"))
-		{
-			velocity.Y += 1;
-		}
 
-		if (Input.IsActionPressed("move_up"))
-		{
-			velocity.Y -= 1;
-		}
-		if (velocity.Length() > 0)
-   		{
-		velocity = velocity.Normalized() * Speed;
-		}	
-		Position += velocity * (float)delta;
-		Position = new Vector2(
-			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
-			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
-		);
+//		if (Input.IsActionPressed("move_down"))
+//		{
+//			velocity.Y += 1;
+//		}
+//
+//		if (Input.IsActionPressed("move_up"))
+//		{
+//			velocity.Y -= 1;
+//		}
+//		if (velocity.Length() > 0)
+//   		{
+//		velocity = velocity.Normalized() * Speed;
+//		}	
+//		Position += velocity * (float)delta;
+//		Position = new Vector2(
+//			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+//			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+//		);
 	
 }
 
