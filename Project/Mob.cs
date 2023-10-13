60c4819ec4ad32bb1ec424500224c68c5295cccf
loprocks <skiboots221@gmail.com>
2023-10-13 17:32:20 -0400

    Added enemy scene and main scene
    Better understanding on how scenes will be utilized moving forward.

---
+++ Project/Mob.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public partial class Mob : RigidBody2D
+{
+//	private void OnVisibleOnScreenNotifier2DScreenExited()
+//{
+//	QueueFree();
+//}
+}
--- Project/Scripts/CharacterBody2D.cs
+++ Project/Scripts/CharacterBody2D.cs
@@ -3,14 +3,17 @@ using System;
 
 public partial class CharacterBody2D : Godot.CharacterBody2D
 {
-	public float gravity = 3000;   //ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	public int gravity = 3000;   //ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 	public int wspeed { get; set; } = 800;
 	public int jumpvelocity { get; set; } = 1200;
+	public bool canJump = false;
+	
 	
 	private Sprite2D _idleSprite;
 	private Sprite2D _rightSprite;
 	private Sprite2D _leftSprite;
 	private Sprite2D _jumpSprite;
+	private Timer _CoyoteTime;
 	
 	public override void _Ready()
 	{
@@ -19,27 +22,44 @@ public partial class CharacterBody2D : Godot.CharacterBody2D
 		_rightSprite = GetNode<Sprite2D>("RightSprite");
 		//_leftSprite = GetNode<Sprite2D>("LeftSprite");
 		_jumpSprite = GetNode<Sprite2D>("JumpSprite");
+	
 	}
 	
 	public override void _PhysicsProcess(double delta)
 	{
 		var velocity = Velocity;
 		
+		if (!IsOnFloor()){ 
+			if(canJump){
+				canJump = false;
+				
+			}
+			velocity.Y += (float)delta * gravity; 
+		}
+		else{
+			canJump = true;
+			if(Input.IsKeyPressed(Key.Space)){
+				velocity.Y = -jumpvelocity; 
+			}
+			//velocity.Y = -jumpvelocity; 
+		}	/*	
 		if (!IsOnFloor())
 			{ velocity.Y += (float)delta * gravity; }
-		
-		if (Input.IsKeyPressed(Key.Space) && IsOnFloor())
-			{ velocity.Y = -jumpvelocity; }
 
-		
-		if (Input.IsActionPressed("move_right"))
-			{ velocity.X = wspeed; }
+		if (Input.IsKeyPressed(Key.Space) && IsOnFloor())
+			{ velocity.Y = -jumpvelocity; }*/
 			
-		else if (Input.IsActionPressed("move_left"))
-			{ velocity.X = -wspeed; }	
+			
+		if (Input.IsActionPressed("move_right")){ 
+			velocity.X = wspeed; 
+		}
+		else if (Input.IsActionPressed("move_left")){ 
+			velocity.X = -wspeed;
+		}	
 		
-		else 
-			{ velocity.X = 0; }
+		else { 
+			velocity.X = 0; 
+		}
 			
 		_UpdateSpriteRenderer(velocity.X,velocity.Y);
 		//_UpdateSpriteRendererY(velocity.Y);
