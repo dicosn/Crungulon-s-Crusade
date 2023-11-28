064cbe67dc74cc591a1d62503f3cc3cd55d4482b
loprocks <skiboots221@gmail.com>
2023-11-28 18:08:00 -0500

    new enemy scene that works half way
    It bounces and does not work with walls yet

--- Project/Scripts/Enemy.cs
+++ Project/Scripts/Enemy.cs
@@ -1,15 +1,37 @@
 using Godot;
 using System;
 
-public partial class Enemy : RigidBody2D
+public partial class Enemy : CharacterBody2D
 {
-	public override void _Ready()
-{
-	//var Sprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-	//string[] mobTypes = animatedSprite2D.SpriteFrames.GetAnimationNames();
-	//animatedSprite2D.Play(mobTypes[GD.Randi() % mobTypes.Length]);
-}
-	private void OnVisibleOnScreenNotifier2DScreenExited()
+public int speed = 750;
+public int gravity = 3000;
+	
+	public void Start(Vector2 position, float direction){
+		Rotation = direction;
+		Position = position;
+		Velocity = new Vector2(speed, 0).Rotated(Rotation);
+	}
+	
+	public override void _PhysicsProcess(double delta){
+ 		Vector2 velocity = Velocity;
+		var collision = MoveAndCollide(velocity * (float)delta);
+		var is_on_wall = false;
+		velocity.Y += (float)delta*gravity;
+		velocity.X = speed;	
+		
+		if (collision != null){
+			velocity = velocity.Bounce(collision.GetNormal());
+			
+			if (is_on_wall == true)	{
+				speed = -speed;
+			}
+			if (collision.GetCollider().HasMethod("Hit")){
+				collision.GetCollider().Call("Hit");
+		}
+	}
+	Velocity = velocity;
+	}			
+	public void OnVisibleOnScreenNotifier2DScreenExited()
 {
 	QueueFree();
 }	
--- Project/Scripts/Mob.cs
+++ Project/Scripts/Mob.cs
@@ -17,69 +17,40 @@ public partial class Mob : RigidBody2D
 	public Marker2D b;
 	[Export]
 	public RigidBody2D mob;
-	//public Vector2 GlobalPosition = startingPosition;
-	//public Vector2 velocity = new Vector2(500,0);
-	//public Vector2 velocity = Vector2.Zero;
-	//public Vector2 Velocity = Vector2.Zero;
 	
 	private float _t = 0.0f;
+	public int speed = 750;
 	
-	//private void _OnReady(){}
-	//private void _OnCollision(){}
-	
-	
-	public void _Ready()
-	{
+	public void _Ready(){	
 		GlobalPosition = a.GlobalPosition;	
-		//GD.Print("Global location",GlobalPosition);	
-		//GD.Print("velocity", Velocity);
-//_rng = new RandomNumberGenerator();
-//
-		//_velocity = new Vector2(10,0);
-		//_OnReady();
-//		//var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 	}
-		public override void _PhysicsProcess(double delta){
-			
-			//var velocity = Velocity;
-			_t += (float)delta * 0.4f;
-			//GD.Print("Physics Process Running");
-			//Vector2 direction = (endingPosition - startingPosition).Normalized();
-			//float distance = startingPosition.DistanceTo(endingPosition);
-			//velocity = direction * speed;
-			//Velocity = velocity;
-			//_t = Mathf.Clamp(_t + speed * delta, 0.0f, 1.0f);
-			//RigidBody2D mob = GetNode<RigidBody2D>("Mob");
-			//float distance = a.GlobalPosition.DistanceTo(b.GlobalPosition);
-			//GD.Print(distance);
-			if (mob.Position != b.Position){
-				mob.Position = a.Position.Lerp(b.Position,_t);
-			}
-			else{
-				_t = 0.0f;
-				mob.Position = b.Position;
-			}
-			//GlobalPosition += velocity * delta;
-			}
-			//KinematicCollision2D collision = MoveAndCollide(_velocity * (float)delta);
-//	
-//			_t += (float)delta * 0.4f;
-			//var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
-//			//var mobEndLocation = GetNode<PathFollow2D>("MobPath/MobEndLocation");
-//   		PathFollow2D a = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
-//			PathFollow2D b = GetNode<PathFollow2D>("MobPath/MobEndLocation");
-//			RigidBody2D Mob = GetNode<RigidBody2D>("Mob");
+	
+//	public void Start(Vector2 position, float direction){
+//		Rotation = direction;
+//		Position = position;
+//		var velocity = Velocity;
+//		velocity = new Vector2(speed, 0).Rotated(Rotation);
+//	}
+	
+	public override void _PhysicsProcess(double delta){
+// 		var collision = MoveAndCollide(velocity * (float)delta);
 //
-//			Mob.Position = a.Position.Lerp(b.Position, _t);
+//		if (collision != null){
+//			velocity = velocity.Bounce(collision.GetNormal());
 //
-//
-//
-////			if (collision != null) {
-////				_OnCollision();
-////				QueueFree();
-////			}
-	//}	
-
-//private void _on_visible_on_screen_notifier_2d_screen_exited() { QueueFree(); }
+//		if (collision.GetCollider().HasMethod("Hit")){
+//			collision.GetCollider().Call("Hit");
+//		}
+//	}			
+//			if (mob.Position <= b.Position){
+//				_t += (float)delta * 0.4f;
+//				mob.Position = a.Position.Lerp(b.Position,_t);
+//			}
+//			else {
+//				_t = 0.0f;
+//				mob.Position = b.Position;
+//				a.Position = b.Position;
+//			}
+	}
 
 }
