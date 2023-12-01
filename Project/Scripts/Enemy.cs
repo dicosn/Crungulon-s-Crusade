90336107fe11789294c742af37995273423ba303
loprocks <skiboots221@gmail.com>
2023-12-01 17:51:05 -0500

    character can bounce off one wall but back and forth

--- Project/Scripts/Enemy.cs
+++ Project/Scripts/Enemy.cs
@@ -5,32 +5,65 @@ public partial class Enemy : CharacterBody2D
 {
 public int speed = 750;
 public int gravity = 3000;
+private Sprite2D _Sprite;
+public bool is_on_wall = false;
+public bool wall = false;
+	
+	public override void _Ready()
+	{
+		_Sprite = GetNode<Sprite2D>("Sprite2D");
+		is_on_wall = false;
+	}	
 	
 	public void Start(Vector2 position, float direction){
 		Rotation = direction;
 		Position = position;
-		Velocity = new Vector2(speed, 0).Rotated(Rotation);
+		Velocity = new Vector2(speed,0).Rotated(Rotation);
 	}
 	
 	public override void _PhysicsProcess(double delta){
  		Vector2 velocity = Velocity;
-		var collision = MoveAndCollide(velocity * (float)delta);
-		var is_on_wall = false;
+		//var collision = MoveAndCollide(velocity * (float)delta);
+		
 		velocity.Y += (float)delta*gravity;
 		velocity.X = speed;	
-		
-		if (collision != null){
-			velocity = velocity.Bounce(collision.GetNormal());
 			
-			if (is_on_wall == true)	{
-				speed = -speed;
+			if (IsOnWall())	{
+				//GD.Print("wallugh");
+				wall = true;
+
+				
 			}
-			if (collision.GetCollider().HasMethod("Hit")){
-				collision.GetCollider().Call("Hit");
+			if (wall == true){
+				velocity.X += -speed;
+				_Sprite.FlipH = true;
+				GD.Print("flip");
+
+			}
+//			else if (wall == false || collision != null){
+//				velocity.X = speed;
+//			}
+		
+		//if (collision != null){
+			//velocity = velocity.Bounce(collision.GetNormal());
+			//velocity = speed;
+
+			//if (collision.GetCollider().HasMethod("Hit")){
+				//collision.GetCollider().Call("Hit");
+		//}
+//	}
+	Velocity = velocity;
+	//FlipEnemy();
+	MoveAndSlide();
+	}			
+	public void FlipEnemy(){
+		if (wall == true){
+			speed = -speed;
+			_Sprite.FlipH = true;
+			GD.Print("flip");
+			wall = false;
 		}
 	}
-	Velocity = velocity;
-	}			
 	public void OnVisibleOnScreenNotifier2DScreenExited()
 {
 	QueueFree();
