f188e27251ff8b9b39304f0de758f027e1f21f16
loprocks <skiboots221@gmail.com>
2023-12-01 23:29:32 -0500

    the enemy moves back and forth!

--- Project/Scripts/Enemy.cs
+++ Project/Scripts/Enemy.cs
@@ -6,13 +6,17 @@ public partial class Enemy : CharacterBody2D
 public int speed = 750;
 public int gravity = 3000;
 private Sprite2D _Sprite;
-public bool is_on_wall = false;
 public bool wall = false;
+public int r_speed = 750;
+public int l_speed = -750;
+
+//cool down time for reversal of direction
+private float move_time = 0.0f;
+private float CD_thresh = 0.016f;
 	
 	public override void _Ready()
 	{
 		_Sprite = GetNode<Sprite2D>("Sprite2D");
-		is_on_wall = false;
 	}	
 	
 	public void Start(Vector2 position, float direction){
@@ -23,35 +27,63 @@ public bool wall = false;
 	
 	public override void _PhysicsProcess(double delta){
  		Vector2 velocity = Velocity;
-		//var collision = MoveAndCollide(velocity * (float)delta);
-		
-		velocity.Y += (float)delta*gravity;
-		velocity.X = speed;	
+		var collision = MoveAndCollide(velocity * (float)delta);	
 			
 			if (IsOnWall())	{
-				//GD.Print("wallugh");
+				GD.Print("wallugh");
 				wall = true;
-
 				
-			}
-			if (wall == true){
-				velocity.X += -speed;
-				_Sprite.FlipH = true;
-				GD.Print("flip");
+				//move_time = 0.0f;
+				
+				if(move_time > CD_thresh ){
+			  		speed = -1 * speed;
+					_Sprite.FlipH = true;
+					GD.Print(velocity, " velocity");
+					}
+					else{
+						move_time = 0f;
+					}
+			}	
+			move_time += (float)delta;
+				//move_time = 0.0f;
+			
+//			else{
+//				move_time += (float)delta;
+//				GD.Print(move_time,"move time");
+//			}
+//			if (wall == true && move_time > CD_thresh && velocity.X == r_speed){
+//				velocity.X = l_speed;
+//				_Sprite.FlipH = true;
+//				GD.Print("flip_right");
+//				move_time = 0.0f;
+//				wall = false;}
+//
+//			else if (wall == true && move_time > CD_thresh && velocity.X == l_speed){
+//				velocity.X = r_speed;
+//
+//				_Sprite.FlipH = true;
+//				GD.Print("flip_left");
+//
+//				move_time = 0.0f;
+//				wall = false;
+//			}
 
-			}
-//			else if (wall == false || collision != null){
+			
+			
+//			else if (wall == false ){
 //				velocity.X = speed;
 //			}
 		
 		//if (collision != null){
-			//velocity = velocity.Bounce(collision.GetNormal());
+			//velocity = velocity.Bounce(collision.GetNormal());}
 			//velocity = speed;
 
 			//if (collision.GetCollider().HasMethod("Hit")){
 				//collision.GetCollider().Call("Hit");
 		//}
 //	}
+	velocity.Y += (float)delta*gravity;
+	velocity.X = speed;
 	Velocity = velocity;
 	//FlipEnemy();
 	MoveAndSlide();
