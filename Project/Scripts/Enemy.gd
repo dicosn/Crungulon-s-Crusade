ff5492f6c47b48d49720c7a30c0394cd406999a9
loprocks <skiboots221@gmail.com>
2024-02-27 17:24:08 -0500

    copied luke's work

--- Project/Scripts/Enemy.gd
+++ Project/Scripts/Enemy.gd
@@ -1,23 +1,20 @@
 extends CharacterBody2D
 var speed = Vector2(150,400)
-var gravity = 3500
+var gravity = 1000
 
 var player
 	
 func _physics_process(delta):
-	var velocity = Vector2(0,0)
+	var velocity = Vector2()
 	var is_jump_interrupted = Input.is_key_pressed(KEY_SPACE) and velocity.y < 0.0
-	var direction = Vector2(0,0)
+	var direction = Vector2(1,1)
 	calculate_move_velocity(direction, is_jump_interrupted)
 	velocity = move_and_slide()
-	set_flip()
-
-func set_flip():
-	if velocity.x == 0:
-		return
-	$Sprite.flip_h = true if velocity.x < 0 else false
+	
 func calculate_move_velocity(direction, is_jump_interrupted):
 	var new_velo = velocity
+	if is_on_wall():
+		speed.x *= -1
 	new_velo.x = speed.x * direction.x
 	new_velo.y += gravity * get_physics_process_delta_time()
 	if direction.y == -1:
@@ -26,12 +23,13 @@ func calculate_move_velocity(direction, is_jump_interrupted):
 		new_velo.y = 0.0
 	
 	velocity = new_velo
-
+	print(velocity)
 func die():
 	queue_free()
 	
 func OnPlayerDetectorBodyEntered(body):
-	pass
+	body.die
+	print("DIE")
 
 #func OnPlayerDetectorBodyExited(body):
 #	if body.name == "Player":
