3f9ab74180563323d3dc6826aa486f1141bc8f7e
dicosn <95448450+dicosn@users.noreply.github.com>
2024-03-15 18:24:23 -0400

    Many fixes

--- Project/Scripts/PController.gd
+++ Project/Scripts/PController.gd
@@ -1,10 +1,23 @@
 extends CharacterBody2D
 
 #horizontal movement stuff
-@export var speed = 1000
+@export var topspeed = 1000
 @export var accel = 50
 
+#gravity
 @export var gravity = 7500
+var gcap = 1500
+
+@export var jumpvelocity = 1000
+
+@export var air_time = 0.0
+@export var ct_thresh = 0.067
+var stopped_jumping = false
+var pause_gravity = false
+
+#jump time vars
+var jump_time = 0.0
+var jt_thresh = 0.3
 
 var _leftSprite: Sprite2D
 var _jumpSprite: Sprite2D
@@ -12,12 +25,21 @@ var _idleSprite: Sprite2D
 var _rightSprite: Sprite2D
 
 func h_input():
+	#more affects the turnaround time
+	var more = 1
 	if(Input.is_action_pressed("move_right")):
-		velocity.x = min(velocity.x + accel, speed)
+		if(velocity.x < 0):
+			more = 2
+		velocity.x = min(velocity.x + accel*more, topspeed)
 	elif(Input.is_action_pressed("move_left")):
-		velocity.x = max(velocity.x - accel, -speed)
+		if(velocity.x > 0):
+			more = 2
+		velocity.x = max(velocity.x - accel*more, -topspeed)
 	else:
-		velocity.x = 0
+		if(velocity.x > 0):
+			velocity.x = max(0, velocity.x - accel)
+		else:
+			velocity.x = min(0, velocity.x + accel)
 	
 
 # Called when the node enters the scene tree for the first time.
