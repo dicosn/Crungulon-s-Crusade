985e84b93fc4905fd1d3f63dab46f581852ab05d
nicdicosimo <95448450+dicosn@users.noreply.github.com>
2024-03-29 19:00:49 -0400

    Fixed jumping

--- Project/Scripts/PController.gd
+++ Project/Scripts/PController.gd
@@ -13,6 +13,7 @@ var gcap = 1500
 @export var air_time = 0.0
 @export var ct_thresh = 0.067
 var stopped_jumping = false
+var can_jump = false
 var pause_gravity = false
 
 #jump time vars
@@ -53,7 +54,7 @@ func _ready():
 func _process(delta):
 	h_input()
 	# If jump button pressed
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_just_pressed("ui_accept"):
 		# And on the floor
 		if is_on_floor():
 			# Then you can jump
@@ -64,11 +65,10 @@ func _process(delta):
 			air_time = ct_thresh
 			jump_time = 0
 			stopped_jumping = false
-
-# If holding down jump
+	# If holding down jump
 	if Input.is_action_pressed("ui_accept") and not stopped_jumping:
 		# And threshold not reached
-		if jump_time < jt_thresh:
+		if jump_time < jt_thresh and not is_on_floor():
 			# Keep jumping
 			velocity.y = -jumpvelocity * 1.25
 			jump_time += delta
@@ -76,13 +76,19 @@ func _process(delta):
 	if not Input.is_action_pressed("ui_accept"):
 		jump_time = jt_thresh
 		stopped_jumping = true
-
+	
+	if is_on_ceiling():
+		stopped_jumping = true
+	
 	if is_on_floor():
 		air_time = 0
 		jump_time = 0
+		#stopped_jumping = true
+		#print("Touched some form of ground")
 	else:
 		if air_time >= ct_thresh:
 			velocity.y += min(gcap, delta * gravity)
 		air_time += delta
-
+		#print("Not on floor")
+	#print("Stopped jumping is ", stopped_jumping )
 	move_and_slide()
